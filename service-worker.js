21130a551f58cd2c66fba09b6bb656eb2c080bad
Denis Ladychenko <dsladychenko@gmail.com>
2020-07-22 17:06:10 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/denisladychenko/Love_Antiques_Website_Frontend/precache-manifest.d4223227beeb5070b2f02c3f69455c63.js"
+  "/precache-manifest.f23d5769e139626d27374a96ae84f053.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/denisladychenko/Love_Antiques_Website_Frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
