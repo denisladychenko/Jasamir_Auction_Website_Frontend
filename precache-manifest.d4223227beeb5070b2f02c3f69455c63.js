0a87d63bec58902c38d127980bae916c81884f6e
Denis Ladychenko <dsladychenko@gmail.com>
2020-07-22 14:33:06 -0400

    Updates

---
+++ precache-manifest.d4223227beeb5070b2f02c3f69455c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73be52dcbeff44082998132f81f28f66",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/index.html"
+  },
+  {
+    "revision": "f29d37152f320959aa96",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/static/css/main.b8abd646.chunk.css"
+  },
+  {
+    "revision": "e80d390b2838738112bb",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/static/js/2.6a35890b.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/static/js/2.6a35890b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29d37152f320959aa96",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/static/js/main.f83cd5ee.chunk.js"
+  },
+  {
+    "revision": "36465c92af45d71a3d53",
+    "url": "/denisladychenko/Love_Antiques_Website_Frontend/static/js/runtime-main.6c87c751.js"
+  }
+]);
